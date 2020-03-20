fa4605f15932fb0c2190788929c093e34d4c74c7
strengthofthemountain <strengthofthemountain@gmail.com>
2020-03-20 13:43:59 -0400

    mvp-ugly

--- starwars/src/components/NameList.js
+++ starwars/src/components/NameList.js
@@ -1,36 +1,38 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
-import raceCard from "./raceCard"
+import NameCard from "./NameCard"
 
-export default function raceList(){
+export default function NameList(){
     const [swapiData, setSwapiData] = useState([])
         
         
     useEffect(() =>{
         axios
             .get(
-                "https://swapi.co/api/species/"
+                `https://swapi.co/api/species/`
             )
             .then(response => {
-                setSwapiData(response);
+                setSwapiData(response.data.results)
+                console.log(swapiData)
                 
             })
             .catch(error=>{
                 console.log(error)
-            });
+            })
         },[])
     
         return(
-            <div className="character-info">
-            {swapiData.map(banana => {
-              return (
-                <raceCard
+            
+            <div className="">
+            {swapiData.map(banana => 
+              
+                <NameCard
                   key={banana.index}
                   name={banana.name}
-                  gender={banana.gender}
+                  homeworld={banana.homeworld}
                 />
-              );
-            })}
+              
+            )}
              </div>
           ) 
     
